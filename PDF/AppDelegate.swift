3646ab42ba87e1dc1abfb5334537859143696f31
Ashton Cochrane <ashtoncochrane@dn-wlan-161-152.sns.otago.ac.nz>
2017-09-29 11:17:23 +1300

    First

---
+++ PDF/AppDelegate.swift
@@ -0,0 +1,31 @@
+//
+//  AppDelegate.swift
+//  PDF
+//
+//  Created by Ashton Cochrane on 19/09/17.
+//  Copyright © 2017 Ashton Cochrane. All rights reserved.
+//
+
+import Quartz
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    @IBAction func openPDF(_ sender: NSToolbarItem) {
+    }
+    
+}
+
