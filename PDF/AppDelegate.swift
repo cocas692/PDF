333d9034762aa1a9506d998c6feca7851ee6d206
Ashton Cochrane <ashtoncochrane96@gmail.com>
2017-09-29 13:06:28 +1300

    almost loading pdf

--- PDF/AppDelegate.swift
+++ PDF/AppDelegate.swift
@@ -12,10 +12,18 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var docs = [URL]()
+    var loaded = false
 
-
+    @IBOutlet weak var window: NSWindow!
+    @IBOutlet weak var viewPDF: PDFView!
+    @IBOutlet weak var openPDF: NSButton!
+    @IBOutlet weak var holdsPDF: NSComboBox!
+    @IBOutlet weak var nextPDF: NSButton!
+    @IBOutlet weak var previousPDF: NSButton!
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        NotificationCenter.default.addObserver(self, selector: #selector(getter: openPDF), name: NSNotification.Name.PDFViewDocumentChanged, object: nil)
     }
     
     
@@ -25,7 +33,49 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     @IBAction func openPDF(_ sender: NSToolbarItem) {
+        let file = NSOpenPanel()
+        file.title = "Choose a PDF file"
+        file.allowedFileTypes = ["pdf"]
+        file.showsHiddenFiles = false
+        file.showsResizeIndicator = true
+        file.allowsMultipleSelection = true
+        file.canChooseFiles = true
+        file.canChooseDirectories = false
+        file.canCreateDirectories = true
+        
+        if (file.runModal() == NSModalResponseOK) {
+            if holdsPDF.numberOfItems > 0 {
+                holdsPDF.removeAllItems()
+                docs = [URL]()
+            }
+            
+            self.docs = file.urls
+            loaded = true
+            
+            // Navigation between different PDFs
+            if docs.count > 1 {
+                nextPDF.isHidden = false
+                previousPDF.isHidden = false
+            } else {
+                nextPDF.isHidden = true
+                previousPDF.isHidden = true
+            }
+            
+            // Set combo box to display the current PDF
+            for url in docs {
+                holdsPDF.addItem(withObjectValue: url.lastPathComponent)
+            }
+            holdsPDF.stringValue = docs[0].lastPathComponent
+            holdsPDF.isHidden = false
+            
+            setPDF(docs[0])
+            
+        }
     }
     
+    func setPDF(_ url: URL) {
+        viewPDF.document = PDFDocument(url: url)
+    }
 }
+
 
